abffa02bb2807f6c6cd2ab3aeb70b061ac0c4204
matheus <mattmvc56@gmail.com>
2022-11-15 11:39:30 -0400

    feat:create app widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      home: const Banner(
-          location: BannerLocation.topEnd,
-          message: 'Matheus Dias',
-          child: MyHomePage(title: 'PokeDex')),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
---
+++ lib/src/app_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const Banner(
+          location: BannerLocation.topEnd,
+          message: 'Matheus Dias',
+          child: MyHomePage(title: 'PokeDex')),
+    );
+  }
+}
