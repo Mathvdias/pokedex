7347ed3e4979d46bab7b22659acd58870b5dec9a
matheus <mattmvc56@gmail.com>
2022-11-17 23:59:53 -0400

    feat: add package icon pokemon

--- lib/src/app/pages/details-page/components/details_success_mobile_component.dart
+++ lib/src/app/pages/details-page/components/details_success_mobile_component.dart
@@ -18,81 +18,83 @@ class DetailsSuccessMobile extends StatelessWidget {
       return convertedValue.toString();
     }
 
-    return Column(
-      children: [
-        Hero(
-          tag: 'imageHero: ${viewModel.pokemonDetails.id}',
-          child: FadeInImage.assetNetwork(
-            image: viewModel.pokemonDetailsStats.sprites!.other!
-                .officialArtwork!.frontDefault
-                .toString(),
-            placeholder: 'assets/images/pokeLoad.gif',
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Hero(
+            tag: 'imageHero: ${viewModel.pokemonDetails.id}',
+            child: FadeInImage.assetNetwork(
+              image: viewModel.pokemonDetailsStats.sprites!.other!
+                  .officialArtwork!.frontDefault
+                  .toString(),
+              placeholder: 'assets/images/pokeLoad.gif',
+            ),
           ),
-        ),
-        Card(
-          color: colorTag(
-              viewModel.pokemonDetailsStats.types![0].type!.name.toString()),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const PageComponent(
-                percentHeight: 0.19,
-                percentWidth: .2,
-              ),
-              Card(
-                elevation: 10,
-                color: Colors.white60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormatterSpecs(
-                          text:
-                              "${convertValue(viewModel.pokemonDetailsStats.height)} m",
-                          description: 'Height',
-                        ),
-                        TextFormatterSpecs(
-                          text:
-                              "${convertValue(viewModel.pokemonDetailsStats.weight)} kg",
-                          description: 'Weight',
-                        ),
-                        const TextFormatterSpecs(
-                          text: ' ♂ ♀ ',
-                          description: 'Gender',
-                        )
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormatterSpecs(
-                          text: toBeginningOfSentenceCase(
-                              viewModel.pokemonDetails.species)!,
-                          description: 'Category',
-                        ),
-                        TextFormatterSpecs(
-                          text: toBeginningOfSentenceCase(viewModel
-                              .pokemonDetailsStats
-                              .abilities?[0]
-                              .ability!
-                              .name)!,
-                          description: 'Abilities',
-                        ),
-                      ],
-                    )
-                  ],
+          Card(
+            color: colorTag(
+                viewModel.pokemonDetailsStats.types![0].type!.name.toString()),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const PageComponent(
+                  percentHeight: 0.19,
+                  percentWidth: .2,
                 ),
-              )
-            ],
+                Card(
+                  elevation: 10,
+                  color: Colors.white60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormatterSpecs(
+                            text:
+                                "${convertValue(viewModel.pokemonDetailsStats.height)} m",
+                            description: 'Height',
+                          ),
+                          TextFormatterSpecs(
+                            text:
+                                "${convertValue(viewModel.pokemonDetailsStats.weight)} kg",
+                            description: 'Weight',
+                          ),
+                          const TextFormatterSpecs(
+                            text: ' ♂ ♀ ',
+                            description: 'Gender',
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormatterSpecs(
+                            text: toBeginningOfSentenceCase(
+                                viewModel.pokemonDetails.species)!,
+                            description: 'Category',
+                          ),
+                          TextFormatterSpecs(
+                            text: toBeginningOfSentenceCase(viewModel
+                                .pokemonDetailsStats
+                                .abilities?[0]
+                                .ability!
+                                .name)!,
+                            description: 'Abilities',
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
