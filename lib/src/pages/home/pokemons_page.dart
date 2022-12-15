5fe705c626dfe452a5f2e85c5463db2f68a9e768
matheus <mattmvc56@gmail.com>
2022-12-15 12:47:30 -0400

    refac: adjust grid view

--- lib/src/pages/home/pokemons_page.dart
+++ lib/src/pages/home/pokemons_page.dart
@@ -65,8 +65,8 @@ class _HomePageState extends State<HomePage>
               toolbarHeight: 60.2,
               shape: const RoundedRectangleBorder(
                 borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(20)),
+                    bottomRight: Radius.circular(25),
+                    bottomLeft: Radius.circular(25)),
               ),
               title: Text(
                 "PokeDex",
@@ -86,7 +86,7 @@ class _HomePageState extends State<HomePage>
             ),
             body: SizedBox.expand(
               child: FractionallySizedBox(
-                  widthFactor: 0.55,
+                  widthFactor: 0.5,
                   alignment: FractionalOffset.bottomCenter,
                   child: AnimatedBuilder(
                       animation: viewModel.state,
@@ -100,103 +100,107 @@ class _HomePageState extends State<HomePage>
                               itemCount: viewModel.listAllPokemon.length,
                               itemBuilder: ((context, index) {
                                 final poke = viewModel.listAllPokemon[index];
-                                return Card(
-                                  shadowColor:
-                                      setCardColor(poke.type1).withOpacity(0.6),
-                                  color: setCardColor(poke.type1),
-                                  child: InkWell(
-                                    borderRadius: BorderRadius.circular(12),
-                                    onTap: () => Navigator.pushNamed(
-                                      context,
-                                      DetailsPokemon.routeName,
-                                      arguments: (poke.id).toString(),
-                                    ),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        Image(
-                                          image: CachedNetworkImageProvider(
-                                            poke.sprite.toString(),
-                                            maxHeight: 100,
-                                            maxWidth: 100,
+                                return FractionallySizedBox(
+                                  widthFactor: 0.9,
+                                  heightFactor: 0.9,
+                                  child: Card(
+                                    shadowColor: setCardColor(poke.type1)
+                                        .withOpacity(0.6),
+                                    color: setCardColor(poke.type1),
+                                    child: InkWell(
+                                      borderRadius: BorderRadius.circular(12),
+                                      onTap: () => Navigator.pushNamed(
+                                        context,
+                                        DetailsPokemon.routeName,
+                                        arguments: (poke.id).toString(),
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Image(
+                                            image: CachedNetworkImageProvider(
+                                              poke.sprite.toString(),
+                                              maxHeight: 100,
+                                              maxWidth: 100,
+                                            ),
+                                            loadingBuilder:
+                                                ((context, child, progress) {
+                                              if (progress == null) {
+                                                return child;
+                                              }
+                                              return Expanded(
+                                                child: Center(
+                                                  child: Image.asset(
+                                                      'assets/images/pokeLoad.gif'),
+                                                ),
+                                              );
+                                            }),
                                           ),
-                                          loadingBuilder:
-                                              ((context, child, progress) {
-                                            if (progress == null) {
-                                              return child;
-                                            }
-                                            return Expanded(
-                                              child: Center(
-                                                child: Image.asset(
-                                                    'assets/images/pokeLoad.gif'),
-                                              ),
-                                            );
-                                          }),
-                                        ),
-                                        const Spacer(),
-                                        Text(
-                                          '#${poke.id.toString()}',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subtitle1
-                                              ?.copyWith(
-                                                fontSize: 32,
-                                                color: Colors.black54,
-                                              ),
-                                        ),
-                                        // const Spacer(),
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 6),
-                                          child: FittedBox(
-                                            child: Text(
-                                              '${toBeginningOfSentenceCase(poke.name)}',
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .titleMedium
-                                                  ?.copyWith(
-                                                color: Colors.white,
-                                                fontSize: 25,
-                                                shadows: <Shadow>[
-                                                  const Shadow(
-                                                    offset: Offset(2, 2),
-                                                    blurRadius: 7,
-                                                    color: Colors.grey,
-                                                  ),
-                                                ],
+                                          const Spacer(),
+                                          Text(
+                                            '#${poke.id.toString()}',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .subtitle1
+                                                ?.copyWith(
+                                                  fontSize: 32,
+                                                  color: Colors.black54,
+                                                ),
+                                          ),
+                                          // const Spacer(),
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 6),
+                                            child: FittedBox(
+                                              child: Text(
+                                                '${toBeginningOfSentenceCase(poke.name)}',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .titleMedium
+                                                    ?.copyWith(
+                                                  color: Colors.white,
+                                                  fontSize: 25,
+                                                  shadows: <Shadow>[
+                                                    const Shadow(
+                                                      offset: Offset(2, 2),
+                                                      blurRadius: 7,
+                                                      color: Colors.grey,
+                                                    ),
+                                                  ],
+                                                ),
                                               ),
                                             ),
                                           ),
-                                        ),
-                                        const Spacer(
-                                          flex: 2,
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            ChipComponent(poke: poke.type1),
-                                            const SizedBox(
-                                              width: 5,
-                                            ),
-                                            if (poke.type2 != null)
-                                              ChipComponent(poke: poke.type2!),
-                                          ],
-                                        ),
-                                        const Spacer(),
-                                      ],
+                                          const Spacer(
+                                            flex: 2,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              ChipComponent(poke: poke.type1),
+                                              const SizedBox(
+                                                width: 5,
+                                              ),
+                                              if (poke.type2 != null)
+                                                ChipComponent(
+                                                    poke: poke.type2!),
+                                            ],
+                                          ),
+                                          const Spacer(),
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 );
                               }),
                               gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                childAspectRatio:
-                                    1 / (width >= 720 ? 1.75 : 1.5),
-                                crossAxisSpacing: 10,
-                                mainAxisSpacing: 10,
-                                crossAxisCount: 3,
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                childAspectRatio: 3 / 5,
+                                crossAxisSpacing: 5,
+                                mainAxisSpacing: 5,
+                                crossAxisCount: 4,
                               ),
                             ),
                             if (viewModel.state.value == ResultState.loading)
